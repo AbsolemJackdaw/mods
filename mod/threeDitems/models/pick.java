16b982fd728cb9b1db98437d9d2f716956839c39
Axel Cornelis <robbeenaxel@hotmail.com>
2013-06-14 18:21:11 +0200

    fixed derp in model

--- mod/threeDitems/models/pick.java
+++ mod/threeDitems/models/pick.java
@@ -16,44 +16,44 @@ public class pick extends ModelBase
 	public pick()
 	{
 		textureWidth = 32;
-		textureHeight = 32;
-
-		Shape1 = new ModelRenderer(this, 0, 0);
-		Shape1.addBox(0F, 0F, 0F, 1, 1, 15);
-		Shape1.setRotationPoint(0F, 0F, -3F);
-		Shape1.setTextureSize(32, 32);
-		Shape1.mirror = true;
-		setRotation(Shape1, 0F, 0F, 0F);
-		Shape2 = new ModelRenderer(this, 0, 0);
-		Shape2.addBox(0F, 0F, 0F, 4, 2, 2);
-		Shape2.setRotationPoint(-1.5F, -0.5F, 11F);
-		Shape2.setTextureSize(32, 32);
-		Shape2.mirror = true;
-		setRotation(Shape2, 0F, 0F, 0F);
-		Shape3 = new ModelRenderer(this, 0, 4);
-		Shape3.addBox(0F, 0F, 0F, 4, 2, 1);
-		Shape3.setRotationPoint(2F, -0.5F, 11.5F);
-		Shape3.setTextureSize(32, 32);
-		Shape3.mirror = true;
-		setRotation(Shape3, 0F, 0.1745329F, 0F);
-		Shape4 = new ModelRenderer(this, 0, 4);
-		Shape4.addBox(0F, 0F, 0F, 4, 2, 1);
-		Shape4.setRotationPoint(-5F, -0.5F, 10.8F);
-		Shape4.setTextureSize(32, 32);
-		Shape4.mirror = true;
-		setRotation(Shape4, 0F, -0.1745329F, 0F);
-		Shape5 = new ModelRenderer(this, 0, 7);
-		Shape5.addBox(0F, 0F, 0F, 3, 1, 1);
-		Shape5.setRotationPoint(-6.6F, 0F, 9.8F);
-		Shape5.setTextureSize(32, 32);
-		Shape5.mirror = true;
-		setRotation(Shape5, 0F, -0.4363323F, 0F);
-		Shape6 = new ModelRenderer(this, 0, 7);
-		Shape6.addBox(0F, 0F, 0F, 3, 1, 1);
-		Shape6.setRotationPoint(5F, 0F, 11F);
-		Shape6.setTextureSize(32, 32);
-		Shape6.mirror = true;
-		setRotation(Shape6, 0F, 0.4363323F, 0F);
+	    textureHeight = 16;
+	    
+	      Shape1 = new ModelRenderer(this, 0, 0);
+	      Shape1.addBox(0F, 0F, 0F, 1, 1, 15);
+	      Shape1.setRotationPoint(0F, 0F, -3F);
+	      Shape1.setTextureSize(32, 16);
+	      Shape1.mirror = true;
+	      setRotation(Shape1, 0F, 0F, 0F);
+	      Shape2 = new ModelRenderer(this, 0, 0);
+	      Shape2.addBox(0F, 0F, 0F, 4, 2, 2);
+	      Shape2.setRotationPoint(-1.5F, -0.5F, 11F);
+	      Shape2.setTextureSize(32, 16);
+	      Shape2.mirror = true;
+	      setRotation(Shape2, 0F, 0F, 0F);
+	      Shape3 = new ModelRenderer(this, 0, 0);
+	      Shape3.addBox(0F, 0F, 0F, 4, 2, 1);
+	      Shape3.setRotationPoint(2F, -0.5F, 11.5F);
+	      Shape3.setTextureSize(32, 16);
+	      Shape3.mirror = true;
+	      setRotation(Shape3, 0F, 0.1745329F, 0F);
+	      Shape4 = new ModelRenderer(this, 0, 0);
+	      Shape4.addBox(0F, 0F, 0F, 4, 2, 1);
+	      Shape4.setRotationPoint(-5F, -0.5F, 10.8F);
+	      Shape4.setTextureSize(32, 16);
+	      Shape4.mirror = true;
+	      setRotation(Shape4, 0F, -0.1745329F, 0F);
+	      Shape5 = new ModelRenderer(this, 0, 3);
+	      Shape5.addBox(0F, 0F, 0F, 3, 1, 1);
+	      Shape5.setRotationPoint(-6.6F, 0F, 9.8F);
+	      Shape5.setTextureSize(32, 16);
+	      Shape5.mirror = true;
+	      setRotation(Shape5, 0F, -0.4363323F, 0F);
+	      Shape6 = new ModelRenderer(this, 0, 3);
+	      Shape6.addBox(0F, 0F, 0F, 3, 1, 1);
+	      Shape6.setRotationPoint(5F, 0F, 11F);
+	      Shape6.setTextureSize(32, 16);
+	      Shape6.mirror = true;
+	      setRotation(Shape6, 0F, 0.4363323F, 0F);
 	}
 
 	public void render(Entity entity, float f, float f1, float f2, float f3, float f4, float f5)
