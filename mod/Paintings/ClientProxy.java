c5d7351b61eb7a6bf092d2524f3e71f505af7792
Axel Cornelis <robbeenaxel@hotmail.com>
2013-08-16 01:55:59 +0200

    managed imports

--- mod/Paintings/ClientProxy.java
+++ mod/Paintings/ClientProxy.java
@@ -1,8 +1,6 @@
 package Paintings;
 
-import Paintings.config.ConfigFile;
 import net.minecraft.entity.item.EntityPainting;
-import net.minecraftforge.common.EnumHelper;
 import cpw.mods.fml.client.registry.RenderingRegistry;
 
 public class ClientProxy extends CommonProxy{
