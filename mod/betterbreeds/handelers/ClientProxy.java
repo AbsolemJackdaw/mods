bb57d34b9e6989d586c02af924d4c00bebb81634
Axel Cornelis <robbeenaxel@hotmail.com>
2013-09-13 02:21:12 +0200

    added child mod for 3d rendering

--- mod/betterbreeds/handelers/ClientProxy.java
+++ mod/betterbreeds/handelers/ClientProxy.java
@@ -1,69 +1,13 @@
 package betterbreeds.handelers;
 
-import net.minecraft.client.model.ModelChicken;
-import net.minecraft.client.model.ModelCow;
-import net.minecraft.client.model.ModelPig;
+import net.minecraft.client.model.*;
+import net.minecraft.item.Item;
+import net.minecraftforge.client.MinecraftForgeClient;
 import net.minecraftforge.common.MinecraftForge;
 import betterbreeds.ModBreeds;
-import betterbreeds.entity.EntityChicken2;
-import betterbreeds.entity.EntityChicken3;
-import betterbreeds.entity.EntityChicken4;
-import betterbreeds.entity.EntityChicken5;
-import betterbreeds.entity.EntityCow2;
-import betterbreeds.entity.EntityCow3;
-import betterbreeds.entity.EntityCow4;
-import betterbreeds.entity.EntityJelly;
-import betterbreeds.entity.EntityPig2;
-import betterbreeds.entity.EntityPig3;
-import betterbreeds.entity.EntityPig4;
-import betterbreeds.entity.EntityPig5;
-import betterbreeds.entity.EntityPig6;
-import betterbreeds.entity.EntitySheep2;
-import betterbreeds.entity.EntitySheep3;
-import betterbreeds.entity.EntitySheep4;
-import betterbreeds.entity.EntitySheep5;
-import betterbreeds.entity.EntityWolf2;
-import betterbreeds.entity.EntityWolf3;
-import betterbreeds.entity.EntityWolf4;
-import betterbreeds.entity.EntityWolf5;
-import betterbreeds.entity.EntityWolf6;
-import betterbreeds.entity.model.ModelChicken2;
-import betterbreeds.entity.model.ModelJelly;
-import betterbreeds.entity.model.ModelSheep10;
-import betterbreeds.entity.model.ModelSheep3;
-import betterbreeds.entity.model.ModelSheep4;
-import betterbreeds.entity.model.ModelSheep5;
-import betterbreeds.entity.model.ModelSheep6;
-import betterbreeds.entity.model.ModelSheep7;
-import betterbreeds.entity.model.ModelSheep8;
-import betterbreeds.entity.model.ModelSheep9;
-import betterbreeds.entity.model.Wolf2;
-import betterbreeds.entity.model.Wolf3;
-import betterbreeds.entity.model.Wolf4;
-import betterbreeds.entity.model.Wolf5;
-import betterbreeds.entity.model.Wolf6;
-import betterbreeds.entity.render.RenderChicken2;
-import betterbreeds.entity.render.RenderChicken3;
-import betterbreeds.entity.render.RenderChicken4;
-import betterbreeds.entity.render.RenderChicken5;
-import betterbreeds.entity.render.RenderCow2;
-import betterbreeds.entity.render.RenderCow3;
-import betterbreeds.entity.render.RenderCow4;
-import betterbreeds.entity.render.RenderJelly;
-import betterbreeds.entity.render.RenderPig2;
-import betterbreeds.entity.render.RenderPig3;
-import betterbreeds.entity.render.RenderPig4;
-import betterbreeds.entity.render.RenderPig5;
-import betterbreeds.entity.render.RenderPig6;
-import betterbreeds.entity.render.RenderSheep2;
-import betterbreeds.entity.render.RenderSheep3;
-import betterbreeds.entity.render.RenderSheep4;
-import betterbreeds.entity.render.RenderSheep5;
-import betterbreeds.entity.render.RenderWolf2;
-import betterbreeds.entity.render.RenderWolf3;
-import betterbreeds.entity.render.RenderWolf4;
-import betterbreeds.entity.render.RenderWolf5;
-import betterbreeds.entity.render.RenderWolf6;
+import betterbreeds.entity.*;
+import betterbreeds.entity.model.*;
+import betterbreeds.entity.render.*;
 import cpw.mods.fml.client.registry.RenderingRegistry;
 import cpw.mods.fml.common.FMLLog;
 
@@ -102,20 +46,6 @@ public class ClientProxy extends CommonProxy
 
 		RenderingRegistry.registerEntityRenderingHandler(EntityJelly.class, new RenderJelly(new ModelJelly(), 0.2F));
 
-		try{
-			threeDitems.RenderItem.inst.addBucket(ModBreeds.Sheepmilk, "/subaraki/3d/bucketMilk.png", 1.0f,0.5f);
-			threeDitems.RenderItem.inst.addBread(ModBreeds.BlackBread, "/subaraki/3d/breadMeat.png", 1.0f,0.5f);
-			threeDitems.RenderItem.inst.addBread(ModBreeds.Sandwich, "/subaraki/3d/breadEgg.png", 1.0f,0.5f);
-			threeDitems.RenderItem.inst.addBread(ModBreeds.SweetBread, "/subaraki/3d/breadSweet.png", 1.0f,0.5f);
-			threeDitems.RenderItem.inst.addBread(ModBreeds.ChocoBrood, "/subaraki/3d/breadC.png", 1.0f,0.5f);
-			threeDitems.RenderItem.inst.addMeat(ModBreeds.horsemeat, "/subaraki/3d/beef.png", 1.0f,0.5f);
-			threeDitems.RenderItem.inst.addMeat(ModBreeds.horsemeatCooked, "/subaraki/3d/beefCooked.png",1.0f,0.5f);
-			threeDitems.RenderItem.inst.addEgg(ModBreeds.EasterEgg, "/subaraki/3d/eggC.png",0.6f,0.3f);
-
-			FMLLog.getLogger().info("3d items are rendered");
-		}catch(Throwable e){
-			FMLLog.getLogger().info("3d items are not rendered");
-		}
 	}
 
 	@Override
